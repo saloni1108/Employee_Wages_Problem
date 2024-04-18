41e480625fe6be7691640e511b4f1eace8737b68
saloni1108 <saloni1108goyal@gmail.com>
2024-04-18 16:46:35 +0530

    [Saloni] Add: Python Script to do the above use cases using switch case

--- employee_wage_prob.py
+++ employee_wage_prob.py
@@ -21,3 +21,21 @@ def parttime_wage(w, t):
     wages = w * t
     print(wages)
 parttime_wage(wage_per_hour, total_hour)
+
+options = {
+    1: "Check employee attendance",
+    2: "calculate dailiy wages",
+    3: "calculate the wages after adding part-time",
+    4: "invalid"
+}
+
+choose = int(input("Enter the case: "))
+if choose == 1:
+    check_attendance()
+elif choose == 2:
+    daily_wages(wage_per_hour, hour_per_day)
+elif choose == 3:
+    parttime_wage(wage_per_hour, total_hour)
+else:
+    print("invalid")
+
