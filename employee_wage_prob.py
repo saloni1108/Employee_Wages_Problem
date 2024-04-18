8cec23d362a48b0405502d4ffb01145fe4d46362
saloni1108 <saloni1108goyal@gmail.com>
2024-04-18 15:59:53 +0530

    [Saloni] Add: Python script to add parttime employee and calculate the wages accordingly

--- employee_wage_prob.py
+++ employee_wage_prob.py
@@ -14,3 +14,10 @@ def daily_wages(w, h):
     wages = w * h
     print(wages)
 daily_wages(wage_per_hour, hour_per_day)
+
+part_time_hour = 4
+total_hour = hour_per_day + part_time_hour
+def parttime_wage(w, t):
+    wages = w * t
+    print(wages)
+parttime_wage(wage_per_hour, total_hour)
