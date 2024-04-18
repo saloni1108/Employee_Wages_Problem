bee77d056a952de0d4783e1fca40529fa5eb29c9
saloni1108 <saloni1108goyal@gmail.com>
2024-04-18 18:02:42 +0530

    [Saloni] Add: Python script to calculate the monthly wage for an employee

--- employee_wage_prob.py
+++ employee_wage_prob.py
@@ -4,6 +4,7 @@ WAGE_PER_HOUR = 20
 HOUR_PER_DAY = 8
 PART_TIME_HOUR = 4
 TOTAL_HOURS = PART_TIME_HOUR + HOUR_PER_DAY
+DAYS = 20
 
 def daily_wages(wage, hour):
     total_wages = wage * hour
@@ -13,16 +14,22 @@ def parttime_wage(wage, total):
     total_wages = wage * total
     print(total_wages)
 
+def monthly_wage(wage, hours, days):
+    monthly_wages = wage * hours * days
+    print(monthly_wages)
+
 def check_attendance():
     attendance_chk = random.randint(0,2)
     match attendance_chk:
         case 1:
             print("Employee is present")
             daily_wages(WAGE_PER_HOUR, HOUR_PER_DAY)
+            monthly_wage(WAGE_PER_HOUR, HOUR_PER_DAY, DAYS)
         case 2: 
             print("Employee is present")
             parttime_wage(WAGE_PER_HOUR, TOTAL_HOURS)
+            monthly_wage(WAGE_PER_HOUR, TOTAL_HOURS, DAYS)
         case _:
             print("Employee is absent")
 check_attendance()
-
+ 
