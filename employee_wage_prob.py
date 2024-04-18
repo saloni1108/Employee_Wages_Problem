6c390b5d47b9ab432feba5cee045170be599e616
saloni1108 <saloni1108goyal@gmail.com>
2024-04-18 17:22:27 +0530

    [Saloni] Refractor: Made use of Match case instead of taking a choice from the user

--- employee_wage_prob.py
+++ employee_wage_prob.py
@@ -1,41 +1,28 @@
 import random
 
+WAGE_PER_HOUR = 20
+HOUR_PER_DAY = 8
+PART_TIME_HOUR = 4
+TOTAL_HOURS = PART_TIME_HOUR + HOUR_PER_DAY
+
+def daily_wages(wage, hour):
+    total_wages = wage * hour
+    print(total_wages)
+
+def parttime_wage(wage, total):
+    total_wages = wage * total
+    print(total_wages)
+
 def check_attendance():
-    n = random.randint(0,1)
-    if n == 1:
-        print("Employee is present")
-    else:
-        print("Employee is absent")
+    attendance_chk = random.randint(0,2)
+    match attendance_chk:
+        case 1:
+            print("Employee is present")
+            daily_wages(WAGE_PER_HOUR, HOUR_PER_DAY)
+        case 2: 
+            print("Employee is present")
+            parttime_wage(WAGE_PER_HOUR, TOTAL_HOURS)
+        case _:
+            print("Employee is absent")
 check_attendance()
-
-wage_per_hour = 20
-hour_per_day = 8
-def daily_wages(w, h):
-    wages = w * h
-    print(wages)
-daily_wages(wage_per_hour, hour_per_day)
-
-part_time_hour = 4
-total_hour = hour_per_day + part_time_hour
-def parttime_wage(w, t):
-    wages = w * t
-    print(wages)
-parttime_wage(wage_per_hour, total_hour)
-
-options = {
-    1: "Check employee attendance",
-    2: "calculate dailiy wages",
-    3: "calculate the wages after adding part-time",
-    4: "invalid"
-}
-
-choose = int(input("Enter the case: "))
-if choose == 1:
-    check_attendance()
-elif choose == 2:
-    daily_wages(wage_per_hour, hour_per_day)
-elif choose == 3:
-    parttime_wage(wage_per_hour, total_hour)
-else:
-    print("invalid")
 
