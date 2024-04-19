146ba45f5ee7a47c976646c90221cd397c79681f
saloni1108 <saloni1108goyal@gmail.com>
2024-04-19 15:26:48 +0530

    Employee Wage Problem

--- employee_wage_prob.py
+++ employee_wage_prob.py
@@ -1,55 +1,59 @@
 import random
 
-WAGE_PER_HOUR = 20
-HOUR_PER_DAY = 8
-PART_TIME_HOUR = 4
-TOTAL_HOURS = PART_TIME_HOUR + HOUR_PER_DAY
-MAX_HOURS = 100
-MAX_DAYS = 20
-DAYS = 20
-DAILY_WAGE = []
-TOTAL_WAGE = []
+class Employee:
+    def __init__(self) -> None:
+        self.WAGE_PER_HOUR = 20
+        self.HOUR_PER_DAY = 8
+        self.PART_TIME_HOUR = 4
+        self.MAX_HOURS = 100
+        self.MAX_DAYS = 20
+        self.DAYS = 20
 
-def daily_wages(wage, hour):
-    total_wages = wage * hour
-    DAILY_WAGE.append(total_wages)
-    return total_wages
+    def daily_wages(self, wage, hour):
+        self.total_wages = wage * hour
+        print("Daily Wage is: ", self.total_wages)
 
-def parttime_wage(wage, total):
-    total_wages = wage * total
-    DAILY_WAGE.append(total_wages)
-    return total_wages
+    def parttime_wage(self, wage, total):
+        self.total_wages = wage * total
+        print("Daily Wage is: ", self.total_wages)
 
-def monthly_wage(wage, hours, days):
-    monthly_wages = wage * hours * days
-    TOTAL_WAGE.append(monthly_wages)
-    return monthly_wages
+    def monthly_wage(self, wage, hours, days):
+        self.monthly_wages = wage * hours * days
+        print("Monthly Wage is: ", self.monthly_wages)
 
-def check_attendance():
-    TOTAL_HOURS_WORKED = 0
-    TOTAL_DAYS_WORKED = 0
-    while TOTAL_HOURS_WORKED < MAX_HOURS and TOTAL_DAYS_WORKED < MAX_DAYS:
-        attendance_chk = random.randint(0,2)
-        match attendance_chk:
-            case 1:
-                print("Employee is present")
-                daily_wages(WAGE_PER_HOUR, HOUR_PER_DAY)
-                TOTAL_HOURS_WORKED += HOUR_PER_DAY
-                TOTAL_DAYS_WORKED += 1
-                monthly_wage(WAGE_PER_HOUR, TOTAL_HOURS_WORKED, TOTAL_DAYS_WORKED)
-            case 2:
-                print("Employee is present")
-                parttime_wage(WAGE_PER_HOUR, TOTAL_HOURS)
-                TOTAL_HOURS_WORKED += TOTAL_HOURS
-                TOTAL_DAYS_WORKED += 1
-                monthly_wage(WAGE_PER_HOUR, TOTAL_HOURS_WORKED, TOTAL_DAYS_WORKED)
-            case _:
-                print("Employee is absent")
-    total_working_hours(TOTAL_HOURS_WORKED)
-    print("Daily Wage is: ", DAILY_WAGE)
-    print("Total Wage is: ", TOTAL_WAGE)
-def total_working_hours(hours):
-    print("total hour: ", hours)
+    def check_attendance(cls):
+        TOTAL_HOURS_WORKED = 0
+        TOTAL_DAYS_WORKED = 0
+        while TOTAL_HOURS_WORKED < cls.MAX_HOURS and TOTAL_DAYS_WORKED < cls.MAX_DAYS:
+            attendance_chk = random.randint(0,2)
+            match attendance_chk:
+                case 1:
+                    print("Employee is present for full time")
+                    TOTAL_DAYS_WORKED += 1
+                    TOTAL_HOURS_WORKED += cls.HOUR_PER_DAY
+                    cls.daily_wages(cls.WAGE_PER_HOUR, cls.HOUR_PER_DAY)
+                case 2:
+                    print("Employee is present but for part-time")
+                    TOTAL_DAYS_WORKED += 1
+                    TOTAL_HOURS_WORKED += cls.PART_TIME_HOUR
+                    cls.daily_wages(cls.WAGE_PER_HOUR, cls.PART_TIME_HOUR)
+                case _:
+                    print("Employee is absent")
+                    TOTAL_DAYS_WORKED += 1
+                    TOTAL_HOURS_WORKED += 0
+        cls.monthly_wage(cls.WAGE_PER_HOUR, TOTAL_HOURS_WORKED, TOTAL_DAYS_WORKED)
+        cls.total_working_hours(TOTAL_HOURS_WORKED)
+    def total_working_hours(self, hours):
+        print("total hour: ", hours)
+John = Employee()
+print("\tJohn's Wage: \n")
+print(John.check_attendance())
 
-check_attendance()
+Aviyana = Employee()
+print("\n\n\t Aviyana's Wage: \n")
+print(Aviyana.check_attendance())
+
+Arianna = Employee()
+print("\n\n\t Arianna's Wage: \n")
+print(Arianna.check_attendance())
  
