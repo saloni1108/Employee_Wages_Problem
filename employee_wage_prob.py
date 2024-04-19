5446f04f149c3af4ddcd736540df7042b7f5a129
saloni1108 <saloni1108goyal@gmail.com>
2024-04-19 10:20:29 +0530

    [Saloni] Add: python script to calculate thewages till the condition of total of 100working hours or the total of 20 working days is completed

--- employee_wage_prob.py
+++ employee_wage_prob.py
@@ -4,6 +4,8 @@ WAGE_PER_HOUR = 20
 HOUR_PER_DAY = 8
 PART_TIME_HOUR = 4
 TOTAL_HOURS = PART_TIME_HOUR + HOUR_PER_DAY
+MAX_HOURS = 100
+MAX_DAYS = 20
 DAYS = 20
 
 def daily_wages(wage, hour):
@@ -19,16 +21,24 @@ def monthly_wage(wage, hours, days):
     print(monthly_wages)
 
 def check_attendance():
+    TOTAL_HOURS_WORKED = 0
+    TOTAL_DAYS_WORKED = 0
     attendance_chk = random.randint(0,2)
     match attendance_chk:
         case 1:
             print("Employee is present")
             daily_wages(WAGE_PER_HOUR, HOUR_PER_DAY)
-            monthly_wage(WAGE_PER_HOUR, HOUR_PER_DAY, DAYS)
-        case 2: 
+            while TOTAL_HOURS_WORKED < MAX_HOURS or TOTAL_DAYS_WORKED < MAX_DAYS:
+                TOTAL_HOURS_WORKED += HOUR_PER_DAY
+                TOTAL_DAYS_WORKED += 1
+            monthly_wage(WAGE_PER_HOUR, TOTAL_HOURS_WORKED, TOTAL_DAYS_WORKED)
+        case 2:
             print("Employee is present")
             parttime_wage(WAGE_PER_HOUR, TOTAL_HOURS)
-            monthly_wage(WAGE_PER_HOUR, TOTAL_HOURS, DAYS)
+            while TOTAL_HOURS_WORKED < MAX_HOURS or TOTAL_DAYS_WORKED < MAX_DAYS: 
+                TOTAL_HOURS_WORKED += TOTAL_HOURS
+                TOTAL_DAYS_WORKED += 1
+            monthly_wage(WAGE_PER_HOUR, TOTAL_HOURS_WORKED, TOTAL_DAYS_WORKED)
         case _:
             print("Employee is absent")
 check_attendance()
