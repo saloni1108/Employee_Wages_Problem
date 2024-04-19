ba06d8f198318e33a095d682857cedf4475fe7c3
saloni1108 <saloni1108goyal@gmail.com>
2024-04-19 13:53:28 +0530

    [Saloni] Refractor: Made the change in the previous python scrip in print statements

--- employee_wage_prob.py
+++ employee_wage_prob.py
@@ -3,7 +3,7 @@ import random
 def check_attendance():
     attendance_chk = random.randint(0,2)
     if attendance_chk == 1:
-        print("Employee is present")
+        print("Employee is present for full day")
     else:
         print("Employee is absent")
 check_attendance()
